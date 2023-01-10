746b37314aafaafc2683471ef0f119c062214e13
Nader Mohammed <silenthope2015@gmail.com>
2023-01-10 02:02:06 +0200

    Add Bonus [DEL+MOVE]

---
+++ Actions/Move.cpp
@@ -0,0 +1,43 @@
+#include "Move.h"
+
+#include "..\ApplicationManager.h"
+
+#include "..\GUI\GUI.h"
+
+Move::Move(ApplicationManager* pApp) :Action(pApp)
+{
+
+}
+
+void Move::Execute()
+{
+	GUI* pGUI = pManager->GetGUI();
+	CFigure* figure = pManager->getSelected();
+	
+	if (figure == NULL) {
+		pGUI->PrintMessage("Select the shape you want to reisze");
+		Point point;
+		pGUI->GetPointClicked(point.x, point.y);
+		figure = pManager->GetFigure(point.x, point.y);
+	}
+	
+	if (figure != NULL) {
+		Point point;
+		/**/
+		while (pGUI->pWind->GetButtonState(LEFT_BUTTON, point.x, point.y) == BUTTON_UP) {
+			//sleep(.1);
+			figure->Move(pGUI,point);
+			pGUI->ClearDrawArea();
+			pManager->UpdateInterface();
+		}
+		
+		pGUI->GetPointClicked(point.x, point.y);
+		/**/figure->Move(pGUI, point);
+		pGUI->ClearDrawArea();
+		pManager->UpdateInterface();
+		
+		
+		
+	}
+	
+}
